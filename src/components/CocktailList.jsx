da92c5f8db6d1908ba0815e5b89d3977a67baca9
tristanfluteaux <tristan.fluteaux@gmail.com>
2021-07-22 17:40:30 +0200

    new update

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -4,24 +4,24 @@ import './CocktailList.css';
 
 const CocktailList = () => {
 
-const [cocktails, setCocktails] = useState([]);
+    const [cocktails, setCocktails] = useState([]);
 
-useEffect(() => {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`)
-    .then(response => response.json())
-    .then(data => setCocktails(data.drinks))
-}, []);
-    
-return (
-    <>    
-        <h1>Cocktails List</h1>
-            <div className="list-container">   
-             {cocktails.map((cocktail) => (
-                <CocktailCard Key={cocktail.idDrink} {...cocktail} />
+    useEffect(() => {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`)
+            .then(response => response.json())
+            .then(data => setCocktails(data.drinks))
+    }, []);
+
+    return (
+        <>
+            <h1>Cocktails List</h1>
+            <div className="list-container">
+                {cocktails.map((cocktail) => (
+                    <CocktailCard Key={cocktail.idDrink} {...cocktail} />
                 ))
-             }
+                }
             </div>
-    </>
+        </>
     )
 };
 
