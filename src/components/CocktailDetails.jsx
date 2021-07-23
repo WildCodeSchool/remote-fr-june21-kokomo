df7e9a64d67d07fcd0862f37d37b4374efacc1b0
François Doussin <doussin.f@gmail.com>
2021-07-23 10:22:34 +0200

    refactoring ingredients

--- src/components/CocktailDetails.jsx
+++ src/components/CocktailDetails.jsx
@@ -15,24 +15,33 @@ const CocktailDetails = () => {
         .then(response => response.json())
         .then(data => setCocktail(data.drinks[0]))
     }, [])
-        
-    const ingredients = Object.entries(cocktail)
-                .filter(ing => ing[0].includes("Ingredient"))
-                .map(i => i[1])
-    const measure = Object.entries(cocktail)
-                .filter(mea => mea[0].includes("Measure"))
-                .map(m => m[1])
-    
+
+    const getIngredients = (cocktail) => {
+        var ingredients = [];
+        const ingredientsName = Object.entries(cocktail)
+            .filter(ing => ing[0].includes("Ingredient") && ing[1] !== null)
+            .map(i => i[1])
+
+        const measure = Object.entries(cocktail)
+            .filter(mea => mea[0].includes("Measure") && mea[1] !== null)
+            .map(m => m[1])
+
+        ingredientsName.forEach((name, i) => ingredients.push({
+            name: name,
+            measure: measure[i]
+        }));
+
+        return ingredients;
+    }
+
     return (
         <div className="details-container">
             <img src={cocktail.strDrinkThumb} alt="cocktail"/>
             <h1 className="cocktail-name">{cocktail.strDrink}</h1>
             <ul className="ingredients">
-                {ingredients.map((a, b) => a && 
-                <li>
-                    {a} {measure[b]}
-                </li>
-                )}
+                {getIngredients(cocktail).map((ingredient, i) => (
+                    <li key={i}>{ingredient.name} :{ingredient.measure} </li>
+                ))}
             </ul>
             <p className="recipe"></p>
             <NavLink to="/cocktails">Return to cocktails list</NavLink>
