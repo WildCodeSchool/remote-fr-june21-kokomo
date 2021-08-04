8a0ff1755eda0a71b442c2e6a82ba25205a807f8
fdmesquita <fannydemesquita@gmail.com>
2021-08-04 14:20:01 +0200

    modify contact css and jsx but will merge the new version from dev

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,87 +1,102 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './Contact.css';
+import "./Contact.css";
 
 const Contact = () => {
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
-    const [message, setMessage] = useState("")
-    const [send, setSend] = useState([])
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [send, setSend] = useState([]);
 
-    const validateForm = (e) => {
-        e.preventDefault()
-        const result = [" Le message a bien été envoyé avec les infos :", firstName, lastName, email, message]
-        setSend(result)
-    }
+  const validateForm = (e) => {
+    e.preventDefault();
+    const result = [
+      " Le message a bien été envoyé avec les infos :",
+      firstName,
+      lastName,
+      email,
+      message,
+    ];
+    setSend(result);
+  };
 
+  return (
+    <div className='contact-container'>
+      <form className='contact-form'>
+        <div className='question'>
+          <label>
+            <input
+              className='contact-input contact-firstname'
+              type='text'
+              name='firtName'
+              id='firstName'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder='UGOLINI'
+            />
+          </label>
+        </div>
+        <div className='question'>
+          <label>
+            <input
+              className='contact-input'
+              type='text'
+              name='lastName'
+              id='lastName'
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder='Maria'
+            />
+          </label>
+        </div>
+        <div className='question'>
+          <label>
+            <input
+              className='contact-input'
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='ugolini.maria@gmail.com'
+            />
+          </label>
+        </div>
 
-    return (
-        <div className='contact-container'>
-            <form className='contact-form'>
-                <div className='question'>
-                    <label>
-                        <input className='contact-input'
-                            type="text"
-                            name="firtName"
-                            id="firstName"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                            placeholder="UGOLINI"
-                        />
-                    </label>
-                </div>
-                <div className='question'>
-                    <label>
-                        <input className='contact-input'
-                            type="text"
-                            name="lastName"
-                            id="lastName"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            placeholder="Maria"
-                        />
-                    </label>
-                </div>
-                <div className='question'>
-                    <label>
-                        <input className='contact-input'
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="ugolini.maria@gmail.com"
-                        />
-                    </label>
-                </div>
+        <div className='question'>
+          <label htmlFor='c_content'>
+            <h4>Message</h4>
+          </label>
+          <textarea
+            type='Message'
+            name='c_content'
+            id='c_content'
+            onChange={(e) => setMessage(e.target.value)}
+            cols='35'
+            rows='7'
+          ></textarea>
+        </div>
 
-                <div className='question'>
-                    <label htmlFor="c_content">Message</label>
-                    <textarea
-                        type="Message"
-                        name="c_content"
-                        id="c_content"
-                        onChange={(e) => setMessage(e.target.value)}
-                        cols="35"
-                        rows="7"></textarea>
-                </div>
-
-                <div className='question'>
-                    <label>
-                        <input
-                            type="submit"
-                            name="submit"
-                            value="Submit"
-                            onClick={(e) => validateForm(e)}
-                        />
-                    </label>
-                </div>
-            </form>
-            <div className='map'>
-                {send.map(info => < div > {info}</div>)}</div>
-        </div >
-    )
-}
+        <div className='question'>
+          <label>
+            <input
+              className='submit-btn'
+              type='submit'
+              name='submit'
+              value='Submit'
+              onClick={(e) => validateForm(e)}
+            />
+          </label>
+        </div>
+      </form>
+      <div className='map'>
+        {send.map((info) => (
+          <div> {info}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
