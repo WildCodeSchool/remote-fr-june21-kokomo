e2d3cbbc8790d9bc613fd9795518940959ea9f9e
Benjamin1er <benjaminj.geron@gmail.com>
2021-08-04 14:46:09 +0200

    Fixed small bugs on Slide Effect

--- src/components/CoeurDeRockeur.jsx
+++ src/components/CoeurDeRockeur.jsx
@@ -11,23 +11,24 @@ function SlideTransition(props) {
 
 const CoeurDeRockeur = (props) => {
 
-    const [fav, setFav] = useState (false);
     const [state, setState] = useState({
         open: false,
+        fav: false,
         Transition: Slide
     });
     
-    
-
     const handleClick = (Transition) => () => {
         setState({
             open: true,
+            fav : !state.fav,
             Transition
         });
-        setFav(!fav)
     };
 
-    const handleClose = () => {
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+          }
         setState({
             ...state,
             open: false,
@@ -39,10 +40,10 @@ const CoeurDeRockeur = (props) => {
     return (
         <div className="heart-icon">
             <Snackbar open={state.open} autoHideDuration={5000} onClose={handleClose} TransitionComponent={state.Transition}>
-                {fav ? <Alert onClose={handleClose} severity="success">You successfuly added {props.name} to your favorites !</Alert> : 
+                {state.fav ? <Alert onClose={handleClose} severity="success">You successfuly added {props.name} to your favorites !</Alert> : 
                 <Alert onClose={handleClose} severity="info">{props.name} is no longer one of your favorite cocktail</Alert>}
             </Snackbar>
-            <FontAwesomeIcon className={fav ? "heart-icon fav" : "heart-icon"} onClick={handleClick(SlideTransition)} icon={ faHeart } size={props.desktop ? "1x" : "3x"} />
+            <FontAwesomeIcon className={state.fav ? "heart-icon fav" : "heart-icon"} onClick={handleClick(SlideTransition)} icon={ faHeart } size={props.desktop ? "1x" : "3x"} />
         </div>
     )
 }
